f2def6ae762b63dadee53943f0cab2a8951af377
AkshayGitHub <akshay.waykos108@gmail.com>
2026-08-24 08:35:09 +0530

    Commit

---
+++ Functionsprogram/Exp1_Function_WithoutParameter.py
@@ -0,0 +1,51 @@
+print("=========Function without Parameter============")
+
+def method1():
+    print("Function start")
+    print("Hello Akshay")
+    print("Hello Python")
+    print("Function End")
+method1()
+method1()
+
+print("=========Function without Parameter(Add)============")
+
+def method2():
+    print("Addition =",10+20)
+
+method2()
+print("=========Function without Parameter(Add)============")
+def Addition():
+    num1=20
+    num2=20
+    sum=num1+num2
+    print("Addition of two no=",sum)
+Addition()
+print("=========Function without Parameter(mul)============")
+
+def multiplication():
+    mul1=10
+    mul2=10
+    total=mul1*mul2
+    print("Multiplication of two no =",total)
+
+multiplication()
+print("=========Function without Parameter(Div)============")
+
+def Dividation():
+    num1=10
+    num2=5
+    total=num1/num2
+    print("Dividation of two no =",total)
+Dividation()
+print("=========Function without Parameter============")
+def Studentdetails():
+    print("Student Name = Akshay")
+    print("Student Age = 30")
+Studentdetails()
+
+
+
+
+
+
---
+++ Functionsprogram/Exp2_Function_withParameter.py
@@ -0,0 +1,67 @@
+print("============Function With Parameter =================")
+
+def studentname(fn,ln):
+    print("Student Name =",fn)
+    print("Student LastName =",ln)
+studentname("Akshay","waykos")
+
+print("=========Function with Parameter(Addition)============")
+
+def addition(num1,num2):
+    sum=num1+num2
+    print("Addition of two no =",sum)
+addition(10,20)
+print("=========Function with Parameter(sub)============")
+
+def Substraction(no1,no2):
+    sub=no1-no2
+    print("Substraction of two no =",sub)
+Substraction(50,20)
+print("=========Function with Parameter(Mul)============")
+
+def Multiplication(num1,num2):
+    mul=num1*num2
+    print("Multiplication of two no =",mul)
+Multiplication(20,10)
+print("======Function with Parameter(Student info)=========")
+
+def Studentinfo(name,age,rollno,grade):
+    print("Student Name =",name)
+    print("Student Age =",age)
+    print("Student rollno =",rollno)
+    print("Student Grade =",grade)
+Studentinfo("Akshay","31",101,"A")
+
+print("=========Function with Parameter(Emp info)============")
+def EmpInfo(EmpName,EmpID,EmpCTC,EmpJD):
+    print("Employee Name =",EmpName)
+    print("Employee ID  = ",EmpID)
+    print("Employee CTC  = ",EmpCTC)
+    print("Employee JoinDate = ",EmpJD)
+EmpInfo("Akshay",3218,8.5,"31/10/2022")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
